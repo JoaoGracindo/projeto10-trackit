aa5e34d84be7446beacf67ce2e5614d282799778
JoaoGracindo <gabrielpessoa97@gmail.com.com>
2022-08-06 13:02:06 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import { Reset } from 'styled-reset'
+
+export default function App (){
+  <React.Fragment>
+    <Reset />
+    <div>Hi, I'm an app!</div>
+  </React.Fragment>
+}
